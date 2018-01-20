27890322674d29d3a041ddbd6dc1a75b5a361da9
unknown <kevin.j.1914@gmail.com>
2018-01-20 01:18:45 -0500

    cleaning up jenkins file debug output, and small reorg of code

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,10 +4,8 @@ pipeline {
     stages {
       stage('Build And Deploy') {
         steps {
-		  sh 'ls -la'
-		  sh 'chmod +x build.sh'
-		  sh 'printenv'
-          sh './build.sh'
+		      sh 'chmod +x scripts/build.sh'
+          sh './scripts/build.sh'
         }
       }
     }
---
+++ scripts/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/sh
+
+set -e
+
+#docker login
+$(aws ecr get-login --region us-east-1 --no-include-email)
+
+DOCKER_REPO="$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO:$REVISION"
+
+echo $DOCKER_REPO
+echo "Building image..."
+docker build -t $DOCKER_REPO .
+echo "Pushing image"
+docker push $DOCKER_REPO
+echo "Updating CFN"
+aws cloudformation update-stack --stack-name $STACK_NAME --use-previous-template --capabilities CAPABILITY_IAM \
+  --parameters ParameterKey=DockerImageURL,ParameterValue=$DOCKER_REPO \
+  ParameterKey=DesiredCapacity,UsePreviousValue=true \
+  ParameterKey=InstanceType,UsePreviousValue=true \
+  ParameterKey=MaxSize,UsePreviousValue=true \
+  ParameterKey=SubnetIDs,UsePreviousValue=true \
+  ParameterKey=VpcId,UsePreviousValue=true
