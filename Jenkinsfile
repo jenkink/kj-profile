99372a919db7bcb2c285a22b274fee777d554c07
jenkink <kevin.j.1914@gmail.com>
2018-03-10 14:52:14 -0500

    updated infrastructure for kj-profile

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,7 +5,7 @@ pipeline {
       stage('Build And Deploy') {
         steps {
 		      sh 'chmod +x scripts/build.sh'
-          sh './scripts/build.sh'
+          sh './scripts/build.sh kj-profile'
         }
       }
     }
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,21 @@
 
 set -e
 
+if [ ! $1 ]; then
+  echo "Usage: $(basename $0) CF_TEMPLATE_DIR "
+  echo
+  echo "        BUILD_TYPE - (CREATE_CHANGE_SET, EXECUTE_CHANGE_SET, DELETE_CHANGE_SET)"
+  echo "        CF_TEMPLATE_DIR - The directory where the cloudformation template lives."
+  echo "        ENV - The environment that this build is for."
+  echo ""
+  exit 1
+fi
+
+#BUILD_TYPE=$1
+CF_TEMPLATE_DIR=$1
+#ENV=$3
+
+TEMPLATE="cloudformation/$CF_TEMPLATE_DIR/compute/$CF_TEMPLATE_DIR.json"
 REVISION=$(echo $GIT_COMMIT|awk '{print substr($0,0,7)}')
 
 #docker login
@@ -14,11 +29,11 @@ echo "Building image..."
 docker build -t $DOCKER_REPO .
 echo "Pushing image"
 docker push $DOCKER_REPO
+echo "putting templates in s3"
+aws s3 cp cloudformation/$CF_TEMPLATE_DIR/ s3://$CF_TEMPLATE_DIR/cf-templates/ --recursive
+echo "pulling down params file"
+aws s3 cp s3://$CF_TEMPLATE_DIR/cf-templates/compute/params_$STACK_NAME.json params.json
 echo "Updating CFN"
-aws cloudformation update-stack --stack-name $STACK_NAME --use-previous-template --capabilities CAPABILITY_IAM \
-  --parameters ParameterKey=DockerImageURL,ParameterValue=$DOCKER_REPO \
-  ParameterKey=DesiredCapacity,UsePreviousValue=true \
-  ParameterKey=InstanceType,UsePreviousValue=true \
-  ParameterKey=MaxSize,UsePreviousValue=true \
-  ParameterKey=SubnetIDs,UsePreviousValue=true \
-  ParameterKey=VpcId,UsePreviousValue=true
+aws cloudformation update-stack --stack-name $STACK_NAME --template-body file://$TEMPLATE --capabilities CAPABILITY_IAM \
+  --parameters file://params.json
+
